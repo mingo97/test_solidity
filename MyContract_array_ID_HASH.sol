d20cb4c564ee18beb979a1c429335385f8fa4b21
mingo97 <62433454+mingo97@users.noreply.github.com>
2020-03-23 11:10:12 +0100

    Semplice contract con array mappato con un ID, come se fosse un DB.
    Uso di funzioni private.

--- MyContract_array_ID_HASH.sol
+++ MyContract_array_ID_HASH.sol
@@ -1,23 +1,28 @@
 /*
 	Mezzanotti Luca
-	Semplice contract con array indicizzato.
+	Semplice contract con array mappato con un ID, come se fosse un DB.
 */
 
 pragma solidity >=0.4.0 <0.7.0;
 
 contract MyContract {
     uint256 public peopleCount;
-    //Array di Person
-    Person[] public people;
-       
+    //Array di Person - con mappatura ID [UINT è la key]
+    mapping(uint => Person) public people;
+    
     struct Person {
+        uint _id;
         string _firstName;
         string _lastName;
     }
     
+    //La prima persona ha indice 1.
     function addPerson(string memory _firstName, string memory _lastName) public {
-        //Aggiunge un elemento all'array
-        people.push(Person(_firstName, _lastName));
+        contaPeople();
+        people[peopleCount] = Person(peopleCount ,_firstName, _lastName);
+    }
+    
+    function contaPeople() internal {
         peopleCount += 1;
     }
     
